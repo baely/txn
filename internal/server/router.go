e84d802bc96f7f9d1e3aaf5b6e5c8bd131eca8a6
Bailey <bailey.butler.234@gmail.com>
2026-03-03 10:17:08 +1100

    Update router.go

--- internal/server/router.go
+++ internal/server/router.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/hostrouter"
 
 	"github.com/baely/txn/internal/common/errors"
@@ -54,6 +55,13 @@ func NewWithConfig(cfg *Config) *Server {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-Up-Authenticity-Signature"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	r.Use(middleware.Timeout(30 * time.Second))
 	r.Mount("/", hr)
 
